853be78e0e1c00dc0f5347385b04572079f8bd69
xhonisheqi-studies <xhonisheqi.business@gmail.com>
2026-04-27 15:47:39 +0200

    aggiunti commenti e costruito server intelligente che si connette sulla porta 80 per avere l'header e il body delle pagine richieste dal client

--- client.py
+++ client.py
@@ -1,23 +1,30 @@
 import socket
 import sys
 
-HOST = sys.argv[1]
+# host su cui connettersi che in questo caso siamo noi
+HOST = '127.0.0.1'
+# una porta casuale sopra le 10000 -- mai sotto 1024 in quanto ci sono altri servizi
 PORT = 50007
+# dominio che dobbiamo cercare e lo mandiamo tramite argv nel terminale
+domain_to_search = sys.argv[1]
 
+# creiamo il socket del client
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connettiamoci
 s.connect((HOST,PORT))
-s.sendall(b'Hello, World!')
+# inviamo il dominio al server
+s.sendall(domain_to_search.encode('utf-8'))
 
-data = s.recv(4096)
-# while True:
-#     chunk = s.recv(4096)
-#     if not chunk: 
-#         break
-#     data += chunk
-
+data = b''
+while True:
+    chunk = s.recv(4096)
+    if not chunk: break
+    data += chunk
+# riceviamo al response 
 data_decoded = data.decode('ISO-8859-1')
 print(data_decoded)
 
+# chiudiamo la sessione del socket
 s.close()
 
 
--- server.py
+++ server.py
@@ -1,19 +1,42 @@
 import socket
 
+# host da bindare
 HOST = ''
+# una porta casuale sopra le 10000 -- mai sotto 1024 in quanto ci sono altri servizi
 PORT = 50007
+# porta di default per servizio HTTP
+HTTP_PORT = 80
 
+# creaiamo il socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # prevebt address already in use
+# bind di host e server
 s.bind((HOST,PORT))
-s.listen(5)
+# impostiamolo in modalità ascolto
+s.listen(1)
 
 
 while True: 
+    # accettiamo la connessione
     client_socket,addr = s.accept()
-    data = b''
+    # riceviamo il domain da parte del client
+    domain = client_socket.recv(4096).decode('utf-8').strip()
+    # creiamo un socket per la richiesta sulla porta 80 da parte del client
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((domain,HTTP_PORT))
+    # componiamo la richiesta 
+    request = f'GET / HTTP/1.1\r\nHost: {domain}\r\nConnection: close\r\n\r\n'
+    # mandiamo la response al server socket
+    remote_socket.sendall(request.encode('utf-8'))
 
-    data = client_socket.recv(4096)
-    client_socket.sendall(data)
+    response = b''
+    while True:
+        chunk = remote_socket.recv(4096)
+        if not chunk: break
+        response += chunk
+    # chiudiamo il remote socket
+    remote_socket.close()
+    # madimao la respone al client socket
+    client_socket.sendall(response)
+    # chiudiamo la sessione del client socket
     client_socket.close()
-
